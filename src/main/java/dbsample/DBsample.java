f69f3c6c4a763759917d822ea8eb7f01082d850e
c0663965 <c0663965@SB-N208-09.lambton.on.ca>
2016-02-04 14:12:18 -0500

    Added Create and Insert

--- src/main/java/dbsample/DBsample.java
+++ src/main/java/dbsample/DBsample.java
@@ -14,6 +14,8 @@ import java.util.logging.Logger;
  */
 public class DBsample {
     
+    private static String table_name="aaabbbccc";
+    
     private static Connection getConnection() throws SQLException{
         
         try {
@@ -22,32 +24,79 @@ public class DBsample {
             Logger.getLogger(DBsample.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        String host=System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-        String port=System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-        String username=System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-        String password=System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-        String name = "dbsample";
-        String url="jdbc:mysql://" + host + ":" + port +"/" + name;
-        
-        return DriverManager.getConnection(url,username,password);
+        String host=System.getenv("OPENSHIFT_MYSQL_DB_HOST"); 
+        String port=System.getenv("OPENSHIFT_MYSQL_DB_PORT"); 
+        String username=System.getenv("OPENSHIFT_MYSQL_DB_USERNAME"); 
+        String password=System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD"); 
+        String name = "dbsample"; 
+        String url="jdbc:mysql://" + host + ":" + port +"/" + name; 
+          
+        return DriverManager.getConnection(url,username,password); 
     }
     
+    public static void createTable()
+    {
+        
+        try
+        {
+            Connection conn=getConnection();
+            
+            Statement stmt=conn.createStatement();
+            
+            String sql = "CREATE TABLE IF NOT EXISTS " + table_name + 
+                         " (id INTEGER not NULL, " +
+                         " first VARCHAR(255), " + 
+                         " sex VARCHAR(255), " + 
+                         " age INTEGER, " + 
+                         " PRIMARY KEY (id));"; 
+            
+            stmt.executeUpdate(sql);
+            
+            conn.close();
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("SQL Exception" + ex.getMessage());
+        }
+    }
+           
+    public static void insertData(){
+         
+        try
+        {
+            Connection conn=getConnection();
+            
+            Statement stmt=conn.createStatement();
+            
+            stmt.executeUpdate("INSERT INTO " + table_name  + " VALUES(1, 'McBeal', 'male',99)," +
+                                                              "(2, 'Lee', 'male',88)," +
+                                                              "(3, 'Gagan', 'male',77)," +
+                                                              "(4, 'Elohor', 'female',66)");
+            conn.close();
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("SQL Exception" + ex.getMessage());
+        }
+    }
+
     public static String getTable(){
         
         String output="";
+
         try {
             Connection conn=getConnection();
             Statement stmt=conn.createStatement();
-            ResultSet rs=stmt.executeQuery("SELECT * FROM people");
+            ResultSet rs=stmt.executeQuery("SELECT * FROM "+ table_name);
             
             while(rs.next()){
-                
-                output+="<h2>"+rs.getString("name")+"</h2>";
-                output+="<p>"+rs.getString("bio")+"</p>";
+                output+="<h1>"+rs.getString("id")+rs.getString("first")+rs.getString("sex")+rs.getString("age")+"</h1>";
             }
             
             conn.close();
-        }catch(SQLException ex){
+        }
+        catch(SQLException ex)
+        {
             output="SQL Exception" + ex.getMessage();
         }
         
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,20 +1,26 @@
 <%-- 
     Document   : index
     Created on : 26-Jan-2016, 4:16:39 PM
-    Author     : c0663965
+    Author     : Kihoon, Lee
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@page import="dbsample.DBsample" %>
 
 <!DOCTYPE html>
+
+
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
     </head>
-    <body>
-        <h1>Hello World!</h1>
-        <%=DBsample.getTable()%>
-    </body>
+    
+        <h1>Hello Hello World!</h1>
+        
+         <%=DBsample.createTable()%>
+         <%=DBsample.insertData()%>
+         <%=DBsample.getTable()%>
+    
 </html>
